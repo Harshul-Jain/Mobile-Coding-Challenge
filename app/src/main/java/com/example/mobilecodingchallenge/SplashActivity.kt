34e93958bc71b310db6912b9f2feb7bedd984918
Harshul Jain <harshuljain.mait.it@gmail.com>
2021-10-10 04:12:52 +0530

    Part 4 Completed

---
+++ app/src/main/java/com/example/mobilecodingchallenge/SplashActivity.kt
@@ -0,0 +1,28 @@
+package com.example.mobilecodingchallenge
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.coroutines.*
+
+class SplashActivity : AppCompatActivity() {
+
+    private val activityScope = CoroutineScope(Dispatchers.Main)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        activityScope.launch {
+            // wait for 2 sec
+            delay(2000)
+            startActivity(Intent(this@SplashActivity, LoginActivity::class.java))
+            finish()
+        }
+    }
+
+    override fun onPause() {
+        activityScope.cancel()
+        super.onPause()
+    }
+}
