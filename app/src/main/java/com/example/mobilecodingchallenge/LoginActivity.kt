c8d05f52748b8b9ace68f1d28ef2308ba51e5a85
Harshul Jain <harshuljain.mait.it@gmail.com>
2021-10-09 05:41:18 +0530

    Part 2 Completed

--- app/src/main/java/com/example/mobilecodingchallenge/LoginActivity.kt
+++ app/src/main/java/com/example/mobilecodingchallenge/LoginActivity.kt
@@ -5,11 +5,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
-    var user_name: String = ""
-    var passw: String = ""
+    var username: String = ""
+    var password: String = ""
+    private val passwordPattern =
+        Regex("(((?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*?)(?=.*!)(?=.*_)(?=.*@)).{5,})")
+    private val usernamePattern = Regex("[A-Za-z0-9]+")
 
     //TextWatcher object
     private var mTextWatcher = object : TextWatcher {
@@ -17,17 +21,42 @@ class LoginActivity : AppCompatActivity() {
         }
 
         override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-
         }
 
         override fun afterTextChanged(s: Editable?) {
             //set value of user_name and passw
-            user_name = username.text.toString()
-            passw = password.text.toString()
+            username = usernameEdt.text.toString()
+            password = passwordEdt.text.toString()
+
             //check whether username or password is empty
-            loginBtn.isEnabled = !(user_name.isEmpty() || passw.isEmpty())
+            val isUsernameValid: Boolean = checkUsername(username)
+            val isPasswordValid: Boolean = checkPassword(password)
+            loginBtn.isEnabled = isPasswordValid && isUsernameValid
+            Log.e("Done", "${loginBtn.isEnabled}")
         }
 
+    }
+
+    private fun checkUsername(userName: String): Boolean {
+        if (userName.isEmpty()) {
+            return false
+        }
+        if (!usernamePattern.matches(userName)) {
+            usernameEdt.error = "Enter proper user name"
+            return false
+        }
+        return true
+    }
+
+    private fun checkPassword(password: String): Boolean {
+        if (password.isEmpty()) {
+            return false
+        }
+        if (password.length < 5 || !passwordPattern.matches(password)) {
+            passwordEdt.error = "Password not meeting the criteria"
+            return false
+        }
+        return true
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,8 +64,8 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
 
         //set listeners
-        username.addTextChangedListener(mTextWatcher)
-        password.addTextChangedListener(mTextWatcher)
+        usernameEdt.addTextChangedListener(mTextWatcher)
+        passwordEdt.addTextChangedListener(mTextWatcher)
 
         //Start Main Activity on Clicking the Login Button
         loginBtn.setOnClickListener {
